2a7393c28e86e4abb853b27a60eb94feabcbe698
sakura <sakura@192.168.110.249>
2019-04-05 10:04:24 +0900

    テストコミット

---
+++ Java.Sakurai.TestGit2/src/jp/ac/nig/sakurai/test/git2/MainTestGit2.java
@@ -0,0 +1,19 @@
+package jp.ac.nig.sakurai.test.git2;
+
+import jp.ac.nig.sakurai.test.git2.main.ServiceMain;
+
+public class MainTestGit2 {
+
+    public MainTestGit2() {
+
+        new ServiceMain();
+    }
+
+    public static void main(String[] args) {
+        new MainTestGit2();
+
+
+
+    }
+
+}
---
+++ Java.Sakurai.TestGit2/src/jp/ac/nig/sakurai/test/git2/main/ServiceMain.java
@@ -0,0 +1,17 @@
+package jp.ac.nig.sakurai.test.git2.main;
+
+public class ServiceMain {
+
+    public ServiceMain() {
+
+        test1();
+
+    }
+
+    private void test1() {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
